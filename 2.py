c2aad3b93a36fc92cbddcf9cd95c927bd4f76155
Victor-Jack <jazeg@wp.pl>
2018-02-25 00:54:56 +0100

    New

--- 2.py
+++ 2.py
@@ -2,4 +2,24 @@ Garage = "Ferrari", "Honda", "Porsche", "Toyota"
 	  
 for each_car in Garage:
 	print(each_car)
-print("DONE")  
+print("DONE\'\n\"\\") 
+
+Zmienna1 = 1
+Zmienna2 = "Mieszadelko"
+Zmienna3 = "15"
+Zmienna4 = 7
+
+print(Zmienna1+Zmienna4)
+Zmienna5 = Zmienna1,  Zmienna2
+print(Zmienna5)
+print(len(str(pow(Zmienna4, 3))))
+Zmienna6 = 'Zmienna4'
+print(Zmienna6)
+Zmienna7 = True
+Zmienna8 = False
+print(Zmienna7, ",",   Zmienna8)
+
+print(Zmienna7 or Zmienna8)
+print(Zmienna7 and Zmienna8)
+print(not Zmienna8)
+
